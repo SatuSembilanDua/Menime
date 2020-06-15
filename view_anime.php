b7c1ee5f6b9145b4f52db9da7d7b0bce77a1c6fb
Willy Faqurokhim <willyfaqurokhim@gmail.com>
2020-06-15 22:03:18 +0700

    src encryption

--- view_anime.php
+++ view_anime.php
@@ -3,10 +3,11 @@
 ?>
 <pre>
 <?= $list_anime['video']; ?>
+<?= d_url($list_anime['video']); ?>
 </pre>
 <h2><?= $anime_txt; ?></h2>
 <br><br>
-<iframe style="width:100%; height:500px;" src="<?= $list_anime['video']; ?>" allowfullscreen="true" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" class="idframe" __idm_frm__="1931"></iframe>
+<iframe style="width:100%; height:500px;" src="<?= d_url($list_anime['video']); ?>" allowfullscreen="true" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" class="idframe" __idm_frm__="1931"></iframe>
 <br><br>
 <?php if(isset($ls_eps)): ?>
 <div class="container nav_bottom">
