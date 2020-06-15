11014c6dfb5d57854a9806578e8b9c4e7c9180d6
Willy Faqurokhim <willyfaqurokhim@gmail.com>
2020-06-15 22:00:28 +0700

    testing

--- view_anime.php
+++ view_anime.php
@@ -1,6 +1,9 @@
 <?php
 	
 ?>
+<pre>
+<?= $list_anime['video']; ?>
+</pre>
 <h2><?= $anime_txt; ?></h2>
 <br><br>
 <iframe style="width:100%; height:500px;" src="<?= $list_anime['video']; ?>" allowfullscreen="true" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" class="idframe" __idm_frm__="1931"></iframe>
