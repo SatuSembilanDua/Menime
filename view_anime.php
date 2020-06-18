053fd23074d6c91afeb5ce0a7039c23c9727a648
Willy Faqurokhim <willyfaqurokhim@gmail.com>
2020-06-18 20:29:16 +0700

    fixing avatar
    - update database
    - change player to html 5 player

--- view_anime.php
+++ view_anime.php
@@ -1,7 +1,14 @@
 
 <h2><?= $anime_txt; ?></h2>
 <br><br>
+<?php if($ml_current['link']=="avatar_the_legend_of_aang"): ?>
+	<video controls width="100%">
+		<source src="<?= $list_anime['video']; ?>" type='video/mp4'/>
+	    <source src="<?= $list_anime['video']; ?>" type='video/webm'/>
+	</video>
+<?php else: ?>
 <iframe style="width:100%; height:500px;" src="<?= $list_anime['video']; ?>" allowfullscreen="true" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" class="idframe" __idm_frm__="1931"></iframe>
+<?php endif; ?>
 <br><br>
 <?php if(isset($ls_eps)): ?>
 <div class="container nav_bottom">
