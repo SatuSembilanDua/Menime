744f74978308fe3fed2d1a852d8acd4be956a24c
WillyFaq <willyfaqurokhim@gmail.com>
2020-12-02 17:54:26 +0700

    Update func.php

--- func.php
+++ func.php
@@ -205,26 +205,36 @@ function anime_info($url){
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER,
-    array(
-        "Upgrade-Insecure-Requests: 1",
-        "User-Agent: Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36",
-        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-        "Accept-Language: en-US,en;q=0.9",
-        "cookie: __cfduid=d2bbe25dc4ecfc4afcca2856489c808311606905853; __atssc=google%3B1; __cf_bm=939048924456d6443f4ad7e7e70aa2ce67fddc85-1606905857-1800-AerjEjBv4UspTU8qLELBFQQtSjs04eTVfXSHJYzFKf0hhH03Gq7A+JNrzDZMNzNWqMaxc7qmgSO7lfcJKS/FJK5cButOCL9fq199jY7G5Yndg/zIA/f5EmhnJBsWqymxhch0L9A0moonq4q27YQB3po=; _ga=GA1.2.1220747440.1606905858; _gid=GA1.2.306610621.1606905858; _gat=1; HstCfa4135177=1606905857965; HstCmu4135177=1606905857965; c_ref_4135177=t.co; HstCnv4135177=1; HstCns4135177=1; PHPSESSID=b838tn50e7banouiqld8evsole; __dtsu=10401606905863A0CD033D995FF7F419; __atuvc=2%7C49; __atuvs=5fc76fff56aed7d4001; HstCla4135177=1606905871642; HstPn4135177=2; HstPt4135177=2"
-    ));
+
+    $headers = [
+	    'X-Apple-Tz: 0',
+	    'X-Apple-Store-Front: 143444,12',
+	    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+	    'Accept-Encoding: gzip, deflate, br',
+	    'Accept-Language: id-ID,id;q=0.9',
+	    'Cache-Control: max-age=0',
+	    'Content-Type: application/x-www-form-urlencoded; charset=utf-8',
+	    'Host: www.oploverz.in',
+	    'Referer: https://www.oploverz.in/series/one-piece-sub-indo/', 
+	    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36',
+	    //'X-MicrosoftAjax: Delta=true'
+	    "Upgrade-Insecure-Requests: 1",
+	    "cookie: __cfduid=d463c2c74b8cb4ad701c41487ef15ba271592223878; PHPSESSID=cq1245ofoi0qab5gun4lqfj365; _ga=GA1.2.205899321.1592223880; _gid=GA1.2.1323666345.1592223880; HstCfa4237846=1592223880537; HstCmu4237846=1592223880537; c_ref_4237846=https%3A%2F%2Fwww.google.com%2F; __dtsu=1040159222388239E79CA78CAA0D19A1; HstCnv4237846=2; _gat_gtag_UA_126097535_3=1; HstCla4237846=1592234566756; HstPn4237846=6; HstPt4237846=14; HstCns4237846=3"
+	];
+
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 	$data = curl_exec($ch);
 	$info = curl_getinfo($ch);
 	$error = curl_error($ch);
 
 
-	echo $data;
 	echo "<pre>";
 	print_r($info);
 	echo "<hr>";
 	print_r($error);
 	echo "</pre>";
+	echo $data;
 
 	curl_close($ch);
 	$dom = new simple_html_dom(null, true, true, DEFAULT_TARGET_CHARSET, true, DEFAULT_BR_TEXT, DEFAULT_SPAN_TEXT);
@@ -293,24 +303,14 @@ function list_anime2($url){
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 	curl_setopt($ch, CURLOPT_PROXY, null);
-	
-
-	$headers = [
-	    'X-Apple-Tz: 0',
-	    'X-Apple-Store-Front: 143444,12',
-	    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-	    'Accept-Encoding: gzip, deflate, br',
-	    'Accept-Language: id-ID,id;q=0.9',
-	    'Cache-Control: max-age=0',
-	    'Content-Type: application/x-www-form-urlencoded; charset=utf-8',
-	    'Host: www.oploverz.in',
-	    'Referer: https://www.oploverz.in/series/one-piece-sub-indo/', 
-	    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36',
-	    //'X-MicrosoftAjax: Delta=true'
-	    "cookie: __cfduid=d463c2c74b8cb4ad701c41487ef15ba271592223878; PHPSESSID=cq1245ofoi0qab5gun4lqfj365; _ga=GA1.2.205899321.1592223880; _gid=GA1.2.1323666345.1592223880; HstCfa4237846=1592223880537; HstCmu4237846=1592223880537; c_ref_4237846=https%3A%2F%2Fwww.google.com%2F; __dtsu=1040159222388239E79CA78CAA0D19A1; HstCnv4237846=2; _gat_gtag_UA_126097535_3=1; HstCla4237846=1592234566756; HstPn4237846=6; HstPt4237846=14; HstCns4237846=3"
-	];
-
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_HTTPHEADER,
+	    array(
+	        "Upgrade-Insecure-Requests: 1",
+	        "User-Agent: Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36",
+	        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+	        "Accept-Language: en-US,en;q=0.9",
+	        "cookie: __cfduid=d463c2c74b8cb4ad701c41487ef15ba271592223878; PHPSESSID=cq1245ofoi0qab5gun4lqfj365; _ga=GA1.2.205899321.1592223880; _gid=GA1.2.1323666345.1592223880; HstCfa4237846=1592223880537; HstCmu4237846=1592223880537; c_ref_4237846=https%3A%2F%2Fwww.google.com%2F; __dtsu=1040159222388239E79CA78CAA0D19A1; HstCnv4237846=2; _gat_gtag_UA_126097535_3=1; HstCla4237846=1592234566756; HstPn4237846=6; HstPt4237846=14; HstCns4237846=3"
+	    ));
 
 	$data = curl_exec($ch);
 	$info = curl_getinfo($ch);
