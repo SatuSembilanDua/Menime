4a31e96acacae6a395efc35cec8f5bcf69b6eebc
WillyFaq <willyfaqurokhim@gmail.com>
2020-12-02 18:18:55 +0700

    Update func.php

--- func.php
+++ func.php
@@ -198,8 +198,6 @@ function anime_info($url){
 	$ch = curl_init();
 	curl_setopt($ch,CURLOPT_URL,$url);
 	
-
-
 	//curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
