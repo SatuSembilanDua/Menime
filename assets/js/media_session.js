16b725abfeaf0c587a3a4283857ede9053c43985
SatuSembilanDua <satusembilandua.168@gmail.com>
2024-02-05 11:10:32 +0700

    cleanup

--- assets/js/media_session.js
+++
@@ -1,9 +0,0 @@
-if ('mediaSession' in navigator) {
-  	navigator.mediaSession.metadata = new MediaMetadata({
-    	artwork: [
-          	{
-            	src: 'https://menime.herokuapp.com/assets/img/icon.png', sizes: '128x128', type: 'image/png'
-            }
-      	]
-    });
-}
